530c7e9f5f21d3e2d38d86ff77a5bc46eee798ee
Yago Zaragoza <yagozaragoza04@gmail.com>
2025-02-19 18:27:11 -0300

    Start Update 2

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -4,22 +4,26 @@ import backgroundImage from "../../public/assets/start_background.jpg";
 function Start() {
   return (
     <div className={styles.divBlack}>
-      <section className={styles.startSection} style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <div className={styles.overlay}>
-        <div className={styles.content}>
-          <h1>
-            <span className={styles.highlight}>UN CAFÉ</span> UNA EXPERIENCIA
+      <section
+        className={styles.startSection}
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      >
+        <div className={styles.overlay}>
+          <div className={styles.content}>
+          <h1 className={styles.playfairHighlight}>
+            <span className={styles.Highlight}>UN CAFÉ</span>
+            <span className={styles.secondHighlight}>UNA EXPERIENCIA</span>
           </h1>
-          <p className={styles.italicText}>
-            <i>Café de especialidad preparado con pasión.</i>
-          </p>
-          <p className={styles.description}>
-            VISÍTANOS Y DISFRUTÁ UNA TAZA PERFECTA EN UN AMBIENTE ACOGEDOR.
-          </p>
-          <button className={styles.menuButton}>Ver menú</button>
+            <p className={styles.italicText}>
+              <span className={styles.Dynalight}>Café</span> <span className={styles.secondDynalight}>de especialidad preparado con pasión</span>
+            </p>
+            <p className={styles.description}>
+              VISÍTANOS Y DISFRUTÁ UNA TAZA PERFECTA EN UN AMBIENTE ACOGEDOR.
+            </p>
+            <button className={styles.menuButton}>Ver menú</button>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
     </div>
   );
 }
