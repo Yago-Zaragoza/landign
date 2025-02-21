85072b23d285e322b14e8c9c836532d03a068427
Nahuel Villaverde <nahuelvillaverdeguzman@gmail.com>
2025-02-21 17:31:51 -0300

    aboutUs comienzo

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,9 +1,23 @@
+import styles from '../components_css/AboutUs.module.css';
+import logoBernardina from '../../public/assets/logo_bernardina.png';
+
+
 function AboutUs() {
     return (
       <div>
-
+        
+          <div className={styles.logoContainer}>
+                    <img src={logoBernardina} alt="Logo" className={styles.logoImage} />
+                    <div className={styles.logoText}>
+                      <span className={styles.logoTextTitle}>BERNARDINA</span>
+                      <span className={styles.logoTextSubTitle}>Sandwich & Coffee</span>
+                    </div>
+          </div>
+        
       </div>
     );
   }
   
   export default AboutUs;
+
+  
