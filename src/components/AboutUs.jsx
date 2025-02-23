fad816a9a17b662693409ed8495dc4947448b7d5
Yago Zaragoza <yagozaragoza04@gmail.com>
2025-02-23 20:55:42 -0300

    About Us Update

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,23 +1,73 @@
-import styles from '../components_css/AboutUs.module.css';
-import logoBernardina from '../../public/assets/logo_bernardina.png';
-
+import styles from "../components_css/AboutUs.module.css";
+import logoBernardina from "../../public/assets/logo_bernardina.png";
 
 function AboutUs() {
-    return (
-      <div>
-        
-          <div className={styles.logoContainer}>
-                    <img src={logoBernardina} alt="Logo" className={styles.logoImage} />
-                    <div className={styles.logoText}>
-                      <span className={styles.logoTextTitle}>BERNARDINA</span>
-                      <span className={styles.logoTextSubTitle}>Sandwich & Coffee</span>
-                    </div>
-          </div>
-        
+  return (
+    <div className={styles.container}>
+      <div className={styles.logoContainer}>
+        <img src={logoBernardina} alt="Logo" className={styles.logoImage} />
+        <div className={styles.logoText}>
+          <span className={styles.logoTextTitle}>BERNARDINA</span>
+          <span className={styles.logoTextSubTitle}>Sandwich & Coffee</span>
+        </div>
       </div>
-    );
-  }
-  
-  export default AboutUs;
+      <div className={styles.lineContainer}>
+        <div className={styles.quoteBox}>
+          <span className={styles.quoteText}>
+            “Todos los días son buenos para regalarse un momento”
+          </span>
+        </div>
+        <div className={styles.lineContent}>
+          <div className={styles.line}>
+            <div className={styles.textContainer}>
+              <p>
+                En Bernardina, creemos que el café es mucho más que una bebida:
+                Es una experiencia que
+              </p>{" "}
+              <p>
+                conecta a las personas y despierta los sentidos. Nuestro
+                objetivo es ofrecer un café de calidad
+              </p>
+              <p>
+                excepcional, cultivado con dedicación y tostado con el máximo
+                cuidado para resaltar sus sabores
+              </p>
+              <p> más puros.</p>
+              <p>
+                Pero la experiencia no estaría completa sin una buena compañía
+                en la mesa. Por eso, en nuestro
+              </p>{" "}
+              <p>
+                menú encontrarás una variedad de opciones dulces y saladas para
+                acompañar tu café: desde
+              </p>
+              <p>
+                {" "}
+                tostados crocantes y esponjosos budines hasta exquisitas tortas
+                caseras y medialunas recién
+              </p>{" "}
+              <p>
+                horneadas. Cada bocado está pensado para complementar y realzar
+                los sabores de nuestro café,
+              </p>{" "}
+              <p>
+                creando el equilibrio perfecto entre aroma, textura y sabor.
+              </p>
+            </div>
+            <div className={styles.footer}>
+              <span className={styles.specialty}>Café de especialidad</span>
+              <img
+                src={logoBernardina}
+                alt="Logo"
+                className={styles.logoImageFooter}
+              />
+              <span className={styles.moments}>Momentos únicos</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-  
+export default AboutUs;
