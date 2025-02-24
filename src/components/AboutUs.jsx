f9efe75e272f49175cbf882d58beb5cf7cfb437f
Nahuel Villaverde <nahuelvillaverdeguzman@gmail.com>
2025-02-24 16:13:07 -0300

    about us change

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -20,37 +20,14 @@ function AboutUs() {
         <div className={styles.lineContent}>
           <div className={styles.line}>
             <div className={styles.textContainer}>
-              <p>
-                En Bernardina, creemos que el café es mucho más que una bebida:
-                Es una experiencia que
-              </p>{" "}
-              <p>
-                conecta a las personas y despierta los sentidos. Nuestro
-                objetivo es ofrecer un café de calidad
-              </p>
-              <p>
-                excepcional, cultivado con dedicación y tostado con el máximo
-                cuidado para resaltar sus sabores
-              </p>
-              <p> más puros.</p>
-              <p>
-                Pero la experiencia no estaría completa sin una buena compañía
-                en la mesa. Por eso, en nuestro
-              </p>{" "}
-              <p>
-                menú encontrarás una variedad de opciones dulces y saladas para
-                acompañar tu café: desde
-              </p>
-              <p>
-                {" "}
-                tostados crocantes y esponjosos budines hasta exquisitas tortas
-                caseras y medialunas recién
-              </p>{" "}
-              <p>
-                horneadas. Cada bocado está pensado para complementar y realzar
-                los sabores de nuestro café,
-              </p>{" "}
-              <p>
+              <p>En Bernardina, creemos que el café es mucho más que una bebida: Es una experiencia que
+                conecta a las personas y despierta los sentidos. Nuestro objetivo es ofrecer un café de calidad
+                excepcional, cultivado con dedicación y tostado con el máximo cuidado para resaltar sus sabores
+                más puros.<br></br>
+                Pero la experiencia no estaría completa sin una buena compañía en la mesa. Por eso, en nuestro
+                menú encontrarás una variedad de opciones dulces y saladas para acompañar tu café: desde
+                tostados crocantes y esponjosos budines hasta exquisitas tortas caseras y medialunas recién
+                horneadas. Cada bocado está pensado para complementar y realzar los sabores de nuestro café,
                 creando el equilibrio perfecto entre aroma, textura y sabor.
               </p>
             </div>
